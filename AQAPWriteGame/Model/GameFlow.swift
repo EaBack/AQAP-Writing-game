52232503633f6fd88b60b8519fd0ca6c14d36e95
EaBack <113344913+EaBack@users.noreply.github.com>
2023-03-28 10:45:40 +0200

    backing up

---
+++ AQAPWriteGame/Model/GameFlow.swift
@@ -0,0 +1,19 @@
+//
+//  GameFlow.swift
+//  AQAPWriteGame
+//
+//  Created by Edgar Backer on 2023-03-28.
+//
+
+import Foundation
+
+class GameFlow {
+    var word : String
+    var userRespons : String
+    
+    init(word: String, userRespons: String){    self.word = word
+        self.userRespons = userRespons
+    }
+}
+
+//var wordlist = []
---
+++ AQAPWriteGame/ViewControl/ViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ViewController.swift
+//  AQAPWriteGame
+//
+//  Created by Edgar Backer on 2023-03-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let segueIdGoToGame = "GoToGame"
+    let segueEndGame = "GameEnded"
+    let segueStartScreen = "GoBackToOne"
+    let seguePlayAgain = "StartNewGame"
+    
+    @IBOutlet weak var rulesLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        rulesLabel.text = "Welcome to AQAP Write game! The game where you write As Quick As Possible. In this game you will be shown words which you need to write in the field below. You have 5 s for each word. 1 point/word or -1 for each mistake. Each game is 10 rounds. Can you make the highscore? "
+        
+    }
+
+    @IBAction func StartGame(_ sender: UIButton) {
+        performSegue(withIdentifier: segueIdGoToGame, sender: self)
+    }
+    
+    @IBAction func EndGame(_ sender: Any, forEvent event: UIEvent) {
+        //configure so that automatically screen 3 opens when game is done
+    }
+    
+    @IBAction func BackToStart(_ sender: UIButton) {
+        performSegue(withIdentifier: segueStartScreen, sender: self)
+    }
+    
+    
+    @IBAction func PlayAgain(_ sender: UIButton) {
+        performSegue(withIdentifier: seguePlayAgain, sender: self)
+    }
+    
+}
+
